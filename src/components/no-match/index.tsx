df612382e7f15c734d322ae7d271e27a1376886a
coder <coder@coders-MacBook-Pro.local>
2020-11-22 15:11:11 +0100

    notfound page

--- src/components/no-match/index.tsx
+++ src/components/no-match/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 import { Link, useLocation } from 'react-router-dom'
 
 export default function NoMatch(props: any) {
+  const [authenticated, setAuthenticated] = useState(
+    !!localStorage.getItem('auth'),
+  )
+  const { status } = useSelector((state: any) => state.login)
   let location = useLocation()
+
+  useEffect(() => {
+    if (status) {
+      setAuthenticated(status.result === 'ok')
+    }
+  }, [status])
 
   return (
     <div className="no-match">
       <h2>
-        <Link to="/login">Login page</Link>
+        {authenticated ? (
+          <Link to="/stock">Stock page</Link>
+        ) : (
+          <Link to="/login">Login page</Link>
+        )}
       </h2>
       <h3>
         No match for <code>{location.pathname}</code>
