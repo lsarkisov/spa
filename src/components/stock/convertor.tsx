0e670288649ddfa9d20c4f2e05dd0406480eaf74
coder <coder@coders-MacBook-Pro.local>
2020-11-24 12:50:51 +0100

    calculator

--- src/components/stock/convertor.tsx
+++ src/components/stock/convertor.tsx
@@ -1,29 +1,64 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { Select, Input, Button } from 'lib'
+import { quoteAction } from 'actions/stock'
+import { QuoteStockAssets } from 'types'
 
 function Convertor() {
-  const [amount, setAmount] = useState<string | undefined>('100')
+  const [quoteNames, setQuoteNames] = useState<string[]>([])
+  const [result, setResult] = useState<number | undefined>()
+  const [from, setFrom] = useState<number>(0)
+  const [to, setTo] = useState<number>(0)
+  const [amount, setAmount] = useState<string | undefined>('')
+  const { quote } = useSelector((state: any) => state.stockQuote)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (!quote) {
+      dispatch(quoteAction({ action: 'quote' }))
+    } else {
+      quote.assets.forEach((q: QuoteStockAssets) => {
+        setQuoteNames((qn) => [...qn, q.asset])
+      })
+    }
+  }, [quote, setQuoteNames, dispatch])
+
+  const onCalculate = () => {
+    if (amount) {
+      const { assets } = quote
+      setResult(
+        (parseFloat(assets[from].quote) / parseFloat(assets[to].quote)) *
+          parseFloat(amount),
+      )
+    }
+  }
 
   return (
     <div className="stock__convertor">
-      <div className="stock__convertor-wrapper">
-        <Input
-          label="Сумма"
-          type="text"
-          value={amount}
-          onChange={setAmount}
-          error={false}
-        />
-        <Select
-          options={['CAD', 'CHF', 'EUR', 'GBP', 'JPY']}
-          onClick={(i) => console.log(i)}
-        />
-        <Select
-          options={['CAD', 'CHF', 'EUR', 'GBP', 'JPY']}
-          onClick={(i) => console.log(i)}
-        />
-        <Button>Рассчитать</Button>
-      </div>
+      {quote && (
+        <div className="stock__convertor-wrapper">
+          <div className="relative">
+            <Input
+              label="Сумма"
+              type="text"
+              value={amount}
+              onChange={setAmount}
+              error={false}
+            />
+            <div className="stock__convertor-result">
+              {result && (
+                <dl>
+                  <dt>Итого</dt>
+                  <dd>{result.toFixed(2)}</dd>
+                </dl>
+              )}
+            </div>
+          </div>
+          <Select options={quoteNames} onClick={setFrom} />
+          <Select options={quoteNames} onClick={setTo} />
+          <Button onClick={onCalculate}>Рассчитать</Button>
+        </div>
+      )}
     </div>
   )
 }
